Model.py: Training algorithm coded but not tested code[x]->test[]->functional test[]->verification[]->validation[] synthetic.py: Various fixes, (primarily default values and continuous signs/symptoms now are generated).  Now doesn't work. code[x]->test[]->functional test[]->verification[]->validation[]

practicum/model.py
@@ -70,8 +70,10 @@ import argparse
 import logging
 from flask import Flask
 from flask.ext.restful import reqparse, Resource, Api, abort
-from collections import defaultdict
+from collections import defaultdict, Counter
 import copy
+import scipy.stats
+from scipy.optimize import curve_fit
 
 ## SETUP
 __author__ = "Gabriel Bassett"
@@ -97,13 +99,105 @@ parser.add_argument('db', help='URL of the neo4j graph database', default=NEODB)
 #    logging.basicConfig(level=args.loglevel)
 # <add other setup here>
 # Connect to database
-G = neo4j.GraphDatabaseService(NEODB)
-g = nx.DiGraph()
-NEODB = args.db
+#G = neo4j.GraphDatabaseService(NEODB)
+#g = nx.DiGraph()
+#NEODB = args.db
 
 
 ## EXECUTION
 class decision_support_system():
+    model = None
+
+
+    def __init__(self):
+        pass
+
+
+    def load_nx_model(self, records):
+        # Graph 1 for all relationships
+        g1 = nx.MultiDiGraph()
+        # Graph 2 for condensed relationships and functional probabilities
+        g2 = nx.DiGraph()
+        # Define dictionaries to store the signs, symptoms, and diagnoses and cardinality
+        signs = defaultdict(int)
+        symptoms = defaultdict(int)
+        diagnoses = defaultdict(int)
+
+
+
+        # for each record, connect signs/symtoms to their respective diagnoses with value
+        for record in records:
+            diagnoses['diagnosis'] += 1
+            for sign in record['signs']:
+                g1.add_edge(sign, record['diagnosis'], value=record['signs'][sign])
+                signs[sign] += 1
+            for symptom in record['symptoms']:
+                g1.add_edge(symptom, record['diagnosis'], value=record['symptoms'][symptom])
+                symptoms[symptom] +=1
+
+
+        # g1 now has a bipartite multi directed graph with edges from signs or symptoms to diagnosis
+        #  bearing the value in a single record
+
+
+        # With G2, we take each sign/symptom relationship and compress multi-edges down to a single edge with a
+        #  probability density distribution function for the edge.  This will be what we use for classifying in the
+        #  model.
+
+        relationships = set(g1.edges())
+        # for each edge pair
+        for relationship in relationships:
+            edges = g1.edge[relationship[0]][relationship[1]]
+            # build a distribution (histogram) of the values
+            values = Counter([edges[x]['value'] for x in edges])
+            x = np.asarray(values.keys())
+            y = np.asarray(values.values())
+            y_norm = y/float(sum(y))
+
+            # Tried using curve_fit (immediately below), but requires binned data so chose norm.fit()
+            #params, pcov = curve_fit(lambda x_data, a, b: scipy.stats.halfnorm.pdf(x_data, loc=a, scale=b), x_in, [n/float(sum(y_in)) for n in y_in])
+            # create distribution
+            # TODO: May not want to use norm.fit() for half-norm data due to cdf not reaching 1.  Instead peg median @ max val @ use curve_fit to plot cdf
+            mu, std = scipy.stats.norm.fit([edges[x]['value'] for x in edges])
+
+            # create the normal fit
+            # if there is data beyond mu +- std, keep it
+            if len([n for n in x < mu - std]) > 0 and len([n for n in x > mu + std]) > 0:
+                normalize = scipy.stats.norm(loc=mu, scale=std).pdf(mu)  # normalize to the mean
+                f = lambda x_data : scipy.stats.norm(loc=mu, scale=std).pdf(x_data) / float(normalize)
+            # else, scrap it and create a halfnorm (or just use the cdf
+            else:
+                normalize = scipy.stats.norm(loc=mu, scale=std).cdf(max(x))  # normalze to the maximum of the data
+                f = lambda x_data: scipy.stats.norm(loc=mu, scale=std).cdf(x_data) / float(normalize)
+
+            # store density function on edge
+            g2.add_edge(relationship[0], relationship[1], value = f)
+
+        # boom.  model.
+        self.model = g2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
     model = None  # in the form of a graph
 
 #    def new_model(self):
@@ -123,10 +217,10 @@ class decision_support_system():
         Takes a string refering to to a neo4j database and save the database handle to the class
         """
         try:
-            self.model = py2neoGraph(model)
+#            self.model = py2neoGraph(model)
             logging.info("Model Loaded.")
             return True
-        except exception as e:
+        except Exception as e:
             logging.error(e.__str__)
             logging.info("Model failed to load.")
             return False
@@ -255,7 +349,7 @@ class decision_support_system():
         # TODO: Increment the total counter
 
         # TODO: Return the 1% and 5% precisions.  (Could we also calculate recall?)
-
+'''
 
 def main():
     logging.info('Beginning main loop.')