6590c1d27a1fe32e2071f22af8fcfb48bc40280b
Gabriel Bassett <gabe@infosecanalytics.com>
2015-02-25 16:13:06 -0600

    test.py: Working on test code.  This is pre-switch to using data frames for scoring predictions. code[x]->test[]->functional test[]->verification[]->validation[]->done[]

--- test.py
+++ test.py
@@ -36,89 +36,77 @@ under the License.
  <No TODO>
 
 """
+
+# this code can be used for manually running t
+"""
+GIT_DIR = "/Users/v685573/OneDrive/Documents/MSIA5243/code/practicum"
+NUM_TRAIN_RECORDS = 1000000  # Number of records to use in training the model.  I'd recommend 100-1000 times the number of diagnoses
+NUM_TEST_RECORDS = 10000  # Number of records to use to use in testing the model
+import imp
+# Generate Synthetic Data
+print "Loading the synthetic data generation module."
+fp, pathname, description = imp.find_module("synthetic", [GIT_DIR])
+synthetic = imp.load_module("synthetic", fp, pathname, description)
+# Create class instance
+print "Creating the synthetic data object 'data' and truth data."
+data = synthetic.test_data()
+# Create records
+print "Creating the synthetic noisy records."
+data.records = data.create_diagnosis_data(data.truth, NUM_TRAIN_RECORDS, data.default)
+
+
+# Train a model based on the synthetic data
+print "Loading the model module."
+fp, pathname, description = imp.find_module("model", [GIT_DIR])
+model = imp.load_module("model", fp, pathname, description)
+# Create decision support system object
+print "Creating the medical decision support system object 'mdss'."
+mdss = model.decision_support_system()
+print "Creating the model."
+mdss.model = mdss.train_nx_model(data.records)
+
+# Use the model to make diagnoses and see how well it did
+print "Generating testing records 'test_records'."
+test_records = data.create_diagnosis_data(data.truth, NUM_TEST_RECORDS, data.default)
+
+print "Diagnosing the test records."
+truth_diagnoses = dict()
+predicted_diagnoses = dict()
+for i in range(len(test_records)):
+    truth_diagnoses[i] = test_records[i].pop('diagnosis')
+    predicted_diagnoses[i] = mdss.query_nx_model(test_records[i])
+"""
+
+
+
 # PRE-USER SETUP
-pass
+import logging
 
 ########### NOT USER EDITABLE ABOVE THIS POINT #################
 
 
 # USER VARIABLES
-NEODB = "http://192.168.121.134:7474/db/data"
-CONFIG_FILE = "/tmp/verum.cfg"
 GIT_DIR = "/Users/v685573/OneDrive/Documents/MSIA5243/code/practicum"
-NUM_TRAIN_RECORDS = 1000000  # Number of records to use in training the model.  I'd recommend 100-1000 times the number of diagnoses
+#NUM_TRAIN_RECORDS = 1000000  # Number of records to use in training the model.  I'd recommend 100-1000 times the number of diagnoses
 NUM_TEST_RECORDS = 10000  # Number of records to use to use in testing the model
+NUM_TRAIN_RECORDS = [100000, 200000, 400000, 600000, 800000, 1000000, 1300000, 1600000, 2500000, 3200000, 5000000]  # list of training record counts to test at
+RESULTS_FILE = "/Users/v685573/OneDrive/Documents/MSIA5243/practicum/results.json"
+LOG_LEVEL = logging.DEBUG
 
 ########### NOT USER EDITABLE BELOW THIS POINT #################
 
 
 ## IMPORTS
-from py2neo import neo4j, cypher
-import networkx as nx
-import argparse
-import logging
-import ConfigParser
+
+
 import imp
+import operator
+import json
+import pandas as pd
 
 ## SETUP
 __author__ = "Gabriel Bassett"
-# Parse Arguments (should correspond to user variables)
-parser = argparse.ArgumentParser(description='This script processes a graph.')
-parser.add_argument('-d', '--debug',
-                    help='Print lots of debugging statements',
-                    action="store_const", dest="loglevel", const=logging.DEBUG,
-                    default=loglevel
-                   )
-parser.add_argument('-v', '--verbose',
-                    help='Be verbose',
-                    action="store_const", dest="loglevel", const=logging.INFO
-                   )
-parser.add_argument('--log', help='Location of log file', default=log)
-parser.add_argument('--config', help='The location of the config file', default=CONFIG_FILE)
-# <add arguments here>
-parser.add_argument('--db', help='URL of the neo4j graph database', default=NEODB)
-args = parser.parse_args()
-# Set up config file
-CONFIG_FILE = args.config
-try:
-  config = ConfigParser.SafeConfigParser()
-  config.readfp(open(CONFIG_FILE))
-  config_exists = True
-except:
-  config_exists = False
-if config_exists:
-    if config.has_section('LOGGING'):
-        if 'level' in config.options('LOGGING'):
-            level = config.get('LOGGING', 'level')
-            if level == 'debug':
-                loglevel = logging.DEBUG
-            elif level == 'verbose':
-                loglevel = logging.INFO
-            else:
-                loglevel = logging.WARNING
-        else:
-            loglevel = logging.WARNING
-        if 'log' in config.options('LOGGING'):
-            log = config.get('LOGGING', 'log')
-        else:
-            log = None
-    if config.has_section('NEO4J'):
-        if 'db' in config.options('NEO4J'):
-            NEODB = config.get('NEO4J', 'db')
-# <add additional config options here>
-
-
-## Set up Logging
-if args.log is not None:
-    logging.basicConfig(filename=args.log, level=args.loglevel)
-else:
-    logging.basicConfig(level=args.loglevel)
-# <add other setup here>
-# Connect to database
-G = neo4j.GraphDatabaseService(NEODB)
-g = nx.DiGraph()
-NEODB = args.db
-
+logging.basicConfig(level=LOG_LEVEL)
 
 ## EXECUTION
 
@@ -135,31 +123,78 @@ def main():
     # Create class instance
     print "Creating the synthetic data object 'data' and truth data."
     data = synthetic.test_data()
-    # Create records
-    print "Creating the synthetic noisy records."
-    data.records = data.create_diagnosis_data(data.truth, NUM_TRAIN_RECORDS, data.default)
 
-
-    # Train a model based on the synthetic data
     print "Loading the model module."
     fp, pathname, description = imp.find_module("model", [GIT_DIR])
     model = imp.load_module("model", fp, pathname, description)
-    # Create decision support system object
-    print "Creating the medical decision support system object 'mdss'."
-    mdss = model.decision_support_system()
-    print "Creating the model."
-    mdss.model = mdss.train_nx_model(data.records)
 
-    # Use the model to make diagnoses and see how well it did
-    print "Generating testing records 'test_records'."
-    test_records = data.create_diagnosis_data(data.truth, NUM_TEST_RECORDS, data.default)
+    data.records = list()
+    results = dict()
 
-    print "Diagnosing the test records."
-    truth_diagnoses = dict()
-    predicted_diagnoses = dict()
-    for i in range(len(test_records)):
-        truth_diagnoses[i] = test_records[i].pop('diagnosis')
-        predicted_diagnoses[i] = mdss.query_nx_model(test_records[i])
+    # Create records
+    print "Starting the record creation and testing loop."
+    for i in range(len(NUM_TEST_RECORDS)):
+        if i == 0:
+            record_increment = NUM_TEST_RECORDS[i]
+        else:
+            record_increment = NUM_TEST_RECORDS[i] - NUM_TEST_RECORDS[i - 1]
+        print "Creating {0} more synthetic noisy records for a total of {1}.".format(record_increment, len(data.records) + record_increment)
+        data.records = data.records + data.create_diagnosis_data(data.truth, record_increment, data.default)
+        # Train a model based on the synthetic data
+        # Create decision support system object
+        print "Creating the medical decision support system object 'mdss'."
+        mdss = model.decision_support_system()
+        print "Creating the model."
+        mdss.model = mdss.train_nx_model(data.records)
+
+        # Use the model to make diagnoses and see how well it did
+        print "Generating {0} testing records 'test_records' for {0} trainging records.".format(NUM_TEST_RECORDS, len(data.records))
+        test_records = data.create_diagnosis_data(data.truth, NUM_TEST_RECORDS, data.default)
+
+        print "Diagnosing the test records."
+        truth_diagnoses = dict()
+        predicted_diagnoses = dict()
+        for i in range(len(test_records)):
+            truth_diagnoses[i] = test_records[i].pop('diagnosis')
+            predicted_diagnoses[i] = mdss.query_nx_model(test_records[i])
+
+        print "Scoring the predictions"
+        # Count number in top 1 and 5
+        results[NUM_TEST_RECORDS[i]] = {
+            'top': 0,
+            'top5': 0,
+            'locs': [],
+            'scores': []
+        }
+        for i in range(len(predicted_diagnoses)):
+            # TODO: Make 'predictions' a matrix or dataframe
+            predictions = sorted(predicted_diagnoses[i].items(), key=operator.itemgetter(1), reverse=True)
+#            predictions = pd.DataFrame(data={"diagnosis":predicted_diagnoses[i].keys(), "score":predicted_diagnoses[i].values()})
+#            predictions.sort(columns='score', ascending=False, inplace=True)
+            if predictions[0][0] == truth_diagnoses[i]:
+                results[NUM_TRAIN_RECORDS[i]]['top'] += 1
+                results[NUM_TRAIN_RECORDS[i]]['top5'] += 1
+            elif truth_diagnoses[i] in [key for key, value in predictions[0:5]]:
+                results[NUM_TRAIN_RECORDS[i]]['top5'] += 1
+            # todo: find location of the diagnosis in the sorted list of diagnoses
+            # TODO: predictions.index(truth_diagnoses[i]) needs to be replaced with a real thing
+            try:
+                loc = predictions.index(truth_diagnoses[i])
+                loc = loc + 1 # because values starting at 0 will confuse people
+            except ValueError:
+                loc = -1
+            results[NUM_TRAIN_RECORDS[i]]['locs'].append(loc)
+            # find the score difference
+            if loc == -1:
+                score_diff = 1
+            else:
+                # TODO: predictions[0]['score'] and predictions[i-1]['score'] need to be replaced with real things
+                score_diff = round((predictions[0]['score']-predictions[i-1]['score'])/float(predictions[0]['score']), 7)
+            results[NUM_TRAIN_RECORDS[i]]['scores'].append(score_diff)
+
+        print "Writing {0} training record results.".format(len(data.records))
+        with open(RESULTS_FILE, 'r') as f:
+            json.dump(results, f)
 
     print "Score the predictions."
     pass # TODO: Notionally going to do this on number of accuracy of top score & accuracy of top 5 scores
