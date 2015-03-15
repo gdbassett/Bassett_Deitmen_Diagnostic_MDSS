23d014233a32a85bc1713d5f9a70f72eccf34bd8
Gabriel Bassett <gabe@infosecanalytics.com>
2015-03-15 18:15:46 -0500

    minor comments in model.py

--- practicum/model.py
+++ practicum/model.py
@@ -164,6 +164,13 @@ class decision_support_system():
             #f when called with a value for the sign/symptom will return a confidence score that should be normalized to 1
 
         # add a type to each node to make coloring/filtering them easier
+        # TODO: add edge w/ type=sign, type=symptom, type=diagnosis, etc
+        # for node in g2.nodes():
+        #    try:
+        #       g2.node[node]['type'] = g1.node[node]['type']
+        #    except KeyError:  # or whatever a missing node error from a graph is
+        #       g2.node[node]['type'] = node.split("_",1)[0]
+        #TODO: remove this and instead carry through the 'type' from previous the injestion graph
         for node in g2.nodes():
             g2.node[node]['type'] = node.split("_",1)[0]
 
@@ -197,12 +204,18 @@ class decision_support_system():
         for record in records:
             diagnoses['diagnosis'] += 1
             for sign in record['signs']:
+                # TODO: add edge w/ type=sign, type=symptom, type=diagnosis, etc
+                # g1.add_node(sign, type='sign')
+                # g1.add_node(record['diagnosis'], type='diagnosis')
                 g1.add_edge(sign, record['diagnosis'], value=record['signs'][sign])
                 signs[sign] += 1
                 if type(record['signs'][sign]) not in [int, float, np.float64]:
                     raise TypeError('Sign/Symptom {0} must be some type of number rather than {1} for {2}'.format(
                         sign, type(record['signs'][sign]), record))
             for symptom in record['symptoms']:
+                # TODO: add edge w/ type=sign, type=symptom, type=diagnosis, etc
+                # g1.add_node(symptom, type='symptom')
+                # g1.add_node(record['diagnosis'], type='diagnosis')
                 g1.add_edge(symptom, record['diagnosis'], value=record['symptoms'][symptom])
                 symptoms[symptom] +=1
 
@@ -312,12 +325,17 @@ class decision_support_system():
         #   get all predecessors with negative relationships
         #   if the negative relationship is in the top (SOME CUTOFF.  SAY 99.7%): drop it
         #   else: record the location in the scored potential diagnoses list
+
+        # add impact score
+
         # Order diagnoses by:
         # 1. It treats diagnoses
         # 1.1 It treats the most (diagnoses to consider) diagnoses
         # 1.2 Lowest negative effect location
         # 2. It treats signs/symptoms
         # 2.1 Sorted by negative effect location
+
+
         # Return potential treatments, ordered, with (sign/sympotm vs diagnosis, # treated, highest negative effect loc)
 
         pass
@@ -338,162 +356,6 @@ class decision_support_system():
         return self.model
 
 
-
-
-
-'''
-    model = None  # in the form of a graph
-
-#    def new_model(self):
-#        """
-#        :return: Nothing
-#
-#        Takes nothing, returns an empty model for training.
-#        """
-#        pass
-
-    def load_model(self, model = NEODB):
-        """
-
-        :param model: Neo4j database URI as string
-        :return: booling success
-
-        Takes a string refering to to a neo4j database and save the database handle to the class
-        """
-        try:
-#            self.model = py2neoGraph(model)
-            logging.info("Model Loaded.")
-            return True
-        except Exception as e:
-            logging.error(e.__str__)
-            logging.info("Model failed to load.")
-            return False
-
-
-    def sign_symptom_training(self, sign_symptom_map):
-        """
-
-        :param sign_symptom_map: dictionary of keys of diagnosis and list of tuples of (signs/symptoms, confidence, value)  (value is only used for continuous variables)
-        :return: booling success
-
-        Takes a single mapping of a sign/symptom to a diagnosis adds it to the Model
-        """
-        pass  # TODO
-        #  TODO : For continuous variables, the total for that variable (regardless of relation to the diagnosis), may need to be kept to normalize Y axis.
-
-        # TODO: (Need a way to categorize signs/symptoms as continuous, factors, or bool)
-
-        # TODO: if needed, create the sign/symptom & label it's type (sign or symptom), it's class (factor, continuous, or bool)
-
-        # TODO: If needed, create a diagnosis
-
-        # TODO: if it doesn't exist, create an edge between the sign/symptom and the diagnosis.  Create initial confidence (factor table, function, or bool)
-        # TODO:  If you cannot incrimentally update, the model will need to keep the entire value set and at the end build a distribution for each of the continuous variables
-
-        # TODO: if it already exists update the confidence (factor table, function, or bool count)
-
-        # NOTE: sign/symptom - diagnosis relationships may be negative in confidence indicating a lacking.  However negative and positive relationships should have their own edges.
-
-
-    def test_symptom_training(self, test_symptom_map):
-        """
-
-        :param test_symptom_map: dictionary of keys of tests and list of tuples of (symptoms, confidence)
-        :return: booling success
-
-        Takes a single mapping of a sign/symptom to a diagnosis adds it to the Model
-        """
-        pass  # TODO
-
-
-    def diagnosis_treatment_training(self, diagnosis_treatment_map):
-        """
-
-        :param diagnosis_treatment_map: dictionary of keys of treatment and values of (diagnosis, impact)
-        :return: booling success
-
-        Takes a single mapping of a treatment to diganosis and adds it to the Model
-        """
-        pass  # TODO
-
-
-    def query(self, signs_symptoms):
-        """
-
-        : param signs_symptoms: A list of signs and/or symptoms
-        : return: a dictionary of form {diagnoses: {diagnosis: score}, tests:{test: score}, treatments:{treatment:score}}
-        """
-
-        d = {}
-        d['diagnosis'] = query_diagnosis(signs_symptoms)
-        d['test'] = query_test(d['diagnosis'])
-        d['treatements'] = query_treatments(d['diagnosis'])
-
-        return d
-
-
-    def query_diagnosis(self, signs_symptoms):
-        """
-
-        : param signs_symptoms: A list of signs and/or symptoms
-        : return: a dictionary of form {diagnosis: score}
-        """
-        pass  # TODO
-        # TODO: Get all signs & symptoms from graph
-
-        # TODO: Get all out edges associated with each sign/symptom
-
-        # TOOD: If necessary, calculate distribution
-
-        # TODO: Normalize to out degree (bool) or feature/distribution (categorical/conditnuous) # May be done during import, but only 
-
-        # TODO: Find the single value of categorical/continuous signs/symptoms as confidence
-
-        # TOOD: sum the confidence by destination of each edge
-
-        # TODO: find all edges into the diagnosis.  Reduce the confidence summation of each diagnosis by the sum of the confidences of it's missing edges.
-
-        # TODO: Return the diagnosis:score dictionary
-
-    def query_test(self, diagnoses):
-        """
-
-        : param diagnoses: a dictionary of diagnoses and scores associated with their likelihood
-        : return: a dictionary of form {test: score}
-        """
-        pass  # TODO
-
-
-    def query_treatments(self, diagnoses):
-        """
-
-        : param diagnoses: a dictionary of diagnoses and scores associated with their likelihood
-        : return: a dictionary of form {treatment:score}
-        """
-        pass  # TODO
-
-
-    def validate_model(sign_symptoms, actual):
-        """
-
-        : param sign_symptoms: a list of lists of signs/symptoms
-        : param actual: a list of actual diagnosis
-        : return: a list of bools representing whether the actual diagnosis was in the top 5 diagnoses
-        """
-        pass  # TODO
-
-        diagnosis = self.query_diagnosis(signs_symptoms)
-
-        # TODO: Sort the diagnosis by score
-
-        # TODO: If the diagnosis is the top diagnosis, incriment the top1 counter
-        
-        # TODO: if the diagnosis is in the top 5, increment the top5 counter
-        
-        # TODO: Increment the total counter
-
-        # TODO: Return the 1% and 5% precisions.  (Could we also calculate recall?)
-'''
 
 def main():
     logging.info('Beginning main loop.')
