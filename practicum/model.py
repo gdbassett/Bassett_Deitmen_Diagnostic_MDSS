6f543fd98e59c8cda4cda3aab485b1004ea5915b
Gabriel Bassett <gabe@infosecanalytics.com>
2015-04-04 15:21:47 -0500

    Added License

--- practicum/model.py
+++ practicum/model.py
@@ -6,8 +6,21 @@
  Copyright 2014 Gabriel Bassett
 
  LICENSE:
- This software is not licenced for use by those other than the author.
-'''
+Copyright 2015 Gabriel Bassett
+
+Licensed under the Apache License, Version 2.0 (the "License") for non-commercial use only;
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+    
+To request a commercial license, please contact the author at gabe@infosecanalytics.com
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
  DESCRIPTION:
  Implmementation of a graph-based medical decision support system.
