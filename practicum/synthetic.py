9c3dfa15b905842e4b16a2cd52dbaff77464bc04
Gabriel Bassett <gabe@infosecanalytics.com>
2015-04-04 15:22:39 -0500

    added license

--- practicum/synthetic.py
+++ practicum/synthetic.py
@@ -13,7 +13,7 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
-    
+
 To request a commercial license, please contact the author at gabe@infosecanalytics.com
 
 Unless required by applicable law or agreed to in writing, software
